0d4e722d8c42e481b6f03f375f454cc9fabcacec
Avinashchugh <Avinashkumar.bscsf21@iba-suk.edu.pk>
2024-03-31 16:52:37 +0500

    added features

--- src/Components/Manager.jsx
+++ src/Components/Manager.jsx
@@ -1,6 +1,28 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { FaRegEye } from "react-icons/fa";
 const Manager = () => {
+  const [form, setForm] = useState({ site: "", username: "", password: "" });
+  const [passwordArray, setPasswordArray] = useState([]);
+  useEffect(() => {
+    let passwords = localStorage.getItem("passwords");
+    if (passwords) {
+      setPasswordArray(JSON.parse(passwords));
+    }
+  }, []);
+
+  const savePassword = () => {
+    console.log(form);
+    setPasswordArray([...passwordArray, form]);
+    localStorage.getItem("passwords", JSON.stringify([...passwordArray, form]));
+    console.log([...passwordArray, form]);
+  };
+  const showPassword = () => {
+    alert("Show the password");
+  };
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
   return (
     <>
       <div className="mycontainer mx-auto">
@@ -17,29 +39,79 @@ const Manager = () => {
           <input
             className="rounded-full border border-green-500 w-full p-4 py-1"
             type="text"
-            name=""
-            id=""
+            value={form.site}
+            placeholder="Enter your website"
+            onChange={handleChange}
+            name="site"
           />
           <div className="flex w-full justify-between gap-8">
             <input
               className="rounded-full border border-green-500 w-full p-4 py-1"
               type="text"
+              placeholder="Enter your username"
+              value={form.username}
+              onChange={handleChange}
+              name="username"
             />
-            <input
-              className="rounded-full border border-green-500 w-full p-4 py-1"
-              type="text"
-            />
+            <div className="relative">
+                <input
+                className="rounded-full border border-green-500 w-full p-4 py-1"
+                type="text"
+                placeholder="Enter password"
+                value={form.password}
+                onChange={handleChange}
+                name="password"
+              />
+              <span
+                className="absolute right-[6px] top-[9px] cursor-pointer"
+                onClick={showPassword}
+              >
+                <FaRegEye />
+              </span>
+            </div>
           </div>
         </div>
         <div className="flex justify-center">
-             <button className="flex justify-center items-center bg-green-400 hover:bg-green-300 rounded-full px-3 py-2">
-                <lord-icon
-                src="https://cdn.lordicon.com/jgnvfzqg.json"
-                trigger="hover"/>
-          Add password
-        </button>
+          <button
+            onClick={savePassword}
+            className="flex justify-center items-center bg-green-400 hover:bg-green-300 rounded-full px-3 py-2"
+          >
+            <lord-icon
+              src="https://cdn.lordicon.com/jgnvfzqg.json"
+              trigger="hover"
+            />
+            Add password
+          </button>
         </div>
-       
+        <div className="passwords">
+          <h2>Your Passwords</h2>
+          <table class="table-auto w-full ">
+            <thead className=" bg-green-800 text-white">
+              <tr>
+                <th className="text-center">Song</th>
+                <th className="text-center">Artist</th>
+                <th className="text-center">Year</th>
+              </tr>
+            </thead>
+            <tbody className="bg-green-100 ">
+              <tr>
+                <td className="text-center w-32">The Sliding Mr. Bones</td>
+                <td className="text-center w-32">Malcolm Lockyer</td>
+                <td className="text-center w-32">1961</td>
+              </tr>
+              <tr>
+                <td className="text-center w-32">Witchy Woman</td>
+                <td className="text-center w-32">The Eagles</td>
+                <td className="text-center w-32">1972</td>
+              </tr>
+              <tr>
+                <td className="text-center w-32">Shining Star</td>
+                <td className="text-center w-32">Earth, Wind, and Fire</td>
+                <td className="text-center w-32">1975</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
