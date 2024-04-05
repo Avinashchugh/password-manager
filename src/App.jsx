1c3b3288cf7fc818022f841c512940293bfb3cec
Avinashchugh <Avinashkumar.bscsf21@iba-suk.edu.pk>
2024-04-05 19:50:00 +0500

    initial  commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./Components/Navbar"
 import Manager from "./Components/Manager"
+import Footer from "./Components/Footer"
 function App() {
   return (
     <div>
      <Navbar/>
      <Manager/>
+     <Footer/>
     </div>
   )
 }
