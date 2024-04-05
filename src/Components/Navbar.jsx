538f8a8cec288171cf1697b01ccf702947f494c7
Avinashchugh <Avinashkumar.bscsf21@iba-suk.edu.pk>
2024-04-05 18:40:05 +0500

    added github button in the nav bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import { FaGithub } from "react-icons/fa";
 const Navbar = () => {
   return (
-    <nav className='bg-slate-800'>
-      <div className='text-white flex flex-wrap justify-between items-center mycontainer p-4 py-5 md:py-3 md:px-6 lg:px-12 xl:px-24'>
-        <div className='logo font-bold text-lg'>
-          <span className='text-green-700'>&lt;</span>
-          <span>Pass</span><span className='text-green-700'>OP/&gt;</span>
+    <nav className="bg-slate-800">
+      <div className="text-white flex flex-wrap justify-between items-center mycontainer p-4 py-5 md:py-3 md:px-6 lg:px-12 xl:px-24">
+        <div className="logo font-bold text-lg">
+          <span className="text-green-700">&lt;</span>
+          <span>Pass</span>
+          <span className="text-green-700">OP/&gt;</span>
         </div>
-        <ul className='flex flex-wrap gap-4 md:gap-8'>
-          <li><a className='hover:font-bold' href="/">Home</a></li>
-          <li><a className='hover:font-bold' href="#">About</a></li>
-          <li><a className='hover:font-bold' href="#">Contact</a></li>
-        </ul>
+        <div>
+          <button className="flex justify-center items-center bg-green-400 hover:bg-green-300 rounded-full px-3 py-1">
+            <span className="mx-1">
+              <FaGithub/>
+            </span>
+            GitHub
+          </button>
+        </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
